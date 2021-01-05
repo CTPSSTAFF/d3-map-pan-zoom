21e4f42b3d9cc3e8845b9e2166c8d3b3b8670568
Ben Krepp <bkrepp@ctps.org>
2021-01-05 15:12:42 -0500

    Pan/zoom implemented using d3.js version 4.

--- app.js
+++ app.js
@@ -10,57 +10,84 @@ CTPS.demoApp.init = function() {
 			$('#blurb').hide();
 			this.value = 'Show description';		
 		}
-	});
-	d3.json("JSON/towns_polym.geojson",
-		function(error, data) { 
-			CTPS.demoApp.generateViz(data); 
-		});
+	}); 
+	CTPS.demoApp.generateViz();
 } // CTPS.demoApp.init()
 
-CTPS.demoApp.generateViz = function(geoJsonData) {	
-
-	var townsFeatureCollection = geoJsonData.features;
-	
+CTPS.demoApp.generateViz = function() {	
 	var width = 960,
 		height = 500;
-		
-	// Define Zoom Function Event Listener
-	function zoomFunction() {
-	  d3.selectAll("path")
-		.attr("transform",
-			"translate(" + d3.event.translate
-			+ ") scale (" + d3.event.scale + ")");
-	}
 
-	// Define Zoom Behavior
-	var zoom = d3.behavior.zoom()
-		.scaleExtent([0.2, 10]) 
-		.on("zoom", zoomFunction);
+	var projection = d3.geoConicConformal()
+		.parallels([41 + 43 / 60, 42 + 41 / 60])
+	    .rotate([71 + 30 / 60, -41 ]);
+		
+	var geoPath = d3.geoPath().projection(projection);
 
 	// SVG Viewport
-	var svgContainer = d3.select("body").append("svg")
+	var svg = d3.select("body")
+				.append("svg")
+				.attr("width", width)
+				.attr("height", height)
+				.style("border", "2px solid steelblue");
+
+	//Define what to do when panning or zooming - event listener
+	var zooming = function(d) {
+		//Log d3.event.transform, so you can see all the goodies inside
+		// console.log(d3.event.transform);
+		
+		//New offset array
+		var offset = [d3.event.transform.x, d3.event.transform.y];
+		//Calculate new scale
+		var newScale = d3.event.transform.k * 2000;
+		//Update projection with new offset and scale
+		projection.translate(offset)
+				  .scale(newScale);
+		//Update all paths
+		svg.selectAll("path")
+			.attr("d", geoPath);
+	}
+	
+	//Then define the zoom behavior
+	var zoom = d3.zoom()
+				 .on("zoom", zooming);
+				 
+	//The center of the state (approximate)
+	var center = projection([-71.4, 42.4]);
+
+	// Create a container in which all zoomable objects will live
+	var map = svg.append("g")
+			.attr("id", "map")
+			.call(zoom)  //Bind the zoom behavior
+			.call(zoom.transform, d3.zoomIdentity  	//Then apply the initial transform.
+				.translate(500,530)					// N.B. The translation vector and 
+				.scale(6));							//      scale factor were determined
+													//      empirically.
+													// N.B. The scale factor is multiplied
+													//      by 2,000 in the zoom handler.
+
+	//Create a new, invisible background rect to catch zoom events	
+	map.append("rect")
+		.attr("x", 0)
+		.attr("y", 0)
 		.attr("width", width)
 		.attr("height", height)
-		.style("border", "2px solid steelblue")
-		.call(zoom);
-
-	var projection = d3.geo.conicConformal()
-		.parallels([41 + 43 / 60, 42 + 41 / 60])
-	    .rotate([71 + 30 / 60, -41 ])
-		.scale([12500]) // N.B. The scale and translation vector were determined empirically.
-		.translate([500,560]);
+		.attr("opacity", 0);
 		
-	var geoPath = d3.geo.path().projection(projection);
-			
-	// Create Massachusetts towns map - all towns symbolized the same way.
-	var maSVG = svgContainer.selectAll("path")
-		.data(townsFeatureCollection)
-		.enter()
-		.append("path")
-		.attr("id", function(d, i) { return d.properties.TOWN_ID; })
-		.attr("d", function(d, i) { return geoPath(d); })	
-		.style("fill", "#edab1e")
-		.append("title")   
-			.text(function(d, i) { return d.properties.TOWN; });
-	
+	//Load in GeoJSON data
+	d3.json("JSON/towns_polym.geojson", function(json) {
+		var townsFeatureCollection = json.features;
+
+		//Bind data and create one path per GeoJSON feature
+		// Create Massachusetts towns map - all towns symbolized the same way.
+		map.selectAll("path")
+		   .data(json.features)
+		   .enter()
+		   .append("path")
+		   .attr("d", geoPath)
+		   .attr("id", function(d, i) { return d.properties.town_id})
+		   .style("fill", "#edab1e")
+		   .append("title")   
+		   .text(function(d, i) { return d.properties.town; })
+	});
 } // CTPS.demoApp.generateViz()
